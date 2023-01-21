847bdacfb43172cb34301bc16a641e7e62c6e179
p-chanmin <pc_m111@naver.com>
2023-01-21 23:39:47 +0900

    #03 Email, Password validate 함수 추가

---
+++ lib/utils/validate.dart
@@ -0,0 +1,25 @@
+/// 이메일 형식을 검증하는 함수(이메일 정규식)
+String? validateEmail(String? value) {
+  if (value == null || value!.trim().isEmpty) {
+    return "이메일을 입력해야 합니다.";
+  } else {
+    String pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regExp = new RegExp(pattern);
+    if (!regExp.hasMatch(value)) {
+      return '잘못된 이메일 형식입니다.';
+    } else {
+      return null;
+    }
+  }
+}
+
+/// 비밀번호를 검증하는 함수(공백, 8자 이상)
+String? validatePassword(String? value) {
+  if (value == null || value!.trim().isEmpty) {
+    return "비밀번호를 입력해야 합니다.";
+  } else if (value.length < 8) {
+    return "비밀번호를 8자 이상 입력하세요.";
+  }
+  return null;
+}
