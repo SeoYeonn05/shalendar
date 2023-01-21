264aabaa0452f4661371dca0187d45a351404452
SeoYeonn05 <seoyeonn051@gmail.com>
2023-01-22 01:20:01 +0900

    todo 클래스 생성

---
+++ lib/data/todo.dart
@@ -0,0 +1,39 @@
+
+import 'dart:ffi';
+
+class Todo{
+  String? todoId;
+  String? title;
+  Int? createdAt;
+  String? calendarId;
+  bool? isComplete;
+
+  Todo({
+    this.todoId,
+    this.title,
+    this.createdAt,
+    this.calendarId,
+    this.isComplete
+  });
+
+  factory Todo.fromJson(Map<String, dynamic> parsedJson){
+    return Todo(
+      todoId: parsedJson[''],
+      title: parsedJson[''],
+      createdAt: parsedJson[''],
+      calendarId: parsedJson[''],
+      isComplete: parsedJson[''],
+    );
+  }
+
+  Map<String, dynamic> toJson(){
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data[''] = todoId;
+    data[''] = title;
+    data[''] = createdAt;
+    data[''] = calendarId;
+    data[''] = isComplete;
+
+    return data;
+  }
+}
