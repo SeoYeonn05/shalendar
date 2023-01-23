60599ca4cf2601dd1fc9e7315efab6fe0f69ae1b
p-chanmin <pc_m111@naver.com>
2023-01-23 23:58:59 +0900

    #03 Login 통신 구현

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -1,5 +1,9 @@
 import "package:flutter/material.dart";
 import 'package:shalendar/screen/register.dart';
+import '../data/ResponseUser.dart';
+import '../data/user.dart';
+import '../network/network_helper.dart';
+import '../utils/snackbar.dart';
 import '../utils/validate.dart';
 
 class Login extends StatefulWidget {
@@ -13,6 +17,7 @@ class _LoginState extends State<Login> {
   final _formKey = GlobalKey<FormState>();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  final _networkHelper = NetworkHelper();
 
   /// 로그인 버튼을 눌렀을 때 동작
   void loginButton() async {
@@ -23,10 +28,30 @@ class _LoginState extends State<Login> {
       print('로그인 버튼 눌림 email: $email, password: $password');
       // 서버통신 부분
 
-      // 로그인 된 페이지로 이동
-      // Navigator.pushAndRemoveUntil(
-      //     context, MaterialPageRoute(builder: (b) => Home()), (route) => false);
+      // User 클래스 생성하여 http 통신 POST
+      var newUser =
+          new User(userId: "", email: email, password: password, name: "");
+      var result = await _networkHelper.post('users/login', newUser.toJson());
 
+      // json 파싱을 통해서 결과 메세지 추출 정상:ok, 값 오류: 메세지, 통신오류 null
+      if (result != null) {
+        // 결과 메시지 출력
+        String resultMsg = ResponseUser.fromJson(result).result;
+        if (resultMsg == "ok") {
+          String? token = ResponseUser.fromJson(result).token;
+          showSnackBar(context, "로그인 완료"); //스낵바 출력
+
+          // 로그인 된 페이지로 이동
+          // Navigator.pushAndRemoveUntil(
+          //     context, MaterialPageRoute(builder: (b) => Home()), (route) => false);
+        } else {
+          // 로그인 실패 시 오류메세지 출력
+          showSnackBar(context, "Email 혹은 password가 틀립니다.");
+        }
+      } else {
+        // 그 외 오류, 통신오류 발생 시 출력
+        showSnackBar(context, "알 수 없는 통신오류가 발생하였습니다.");
+      }
     }
   }
 
