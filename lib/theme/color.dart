73e330052642b3896cedd9b54edd238680f4d801
SeoYeonn05 <seoyeonn051@gmail.com>
2023-01-25 18:07:19 +0900

    colorStyles 생성

--- lib/theme/color.dart
+++ lib/theme/color.dart
@@ -1,3 +1,11 @@
-class Color{
-  // static const Color dark =
+import 'package:flutter/material.dart';
+
+class ColorStyles {
+  static const Color themeYellow = Color(0xffF5FFBB);
+  static const Color themeRed = Color(0xffFFD6D6);
+  static const Color themePink = Color(0xffEAC7FF);
+  static const Color themeBlue = Color(0xffEAC7FF);
+  static const Color themeGreen = Color(0xffBBFFCA);
+  static const Color themeOrange = Color(0xffFFDDAB);
+  static const Color themeBlack = Color(0xffB1B1B1);
 }
